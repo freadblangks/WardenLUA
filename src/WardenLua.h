9d0a08a46459ca8cf6739ef34ac696ba77754f37
AnchyDev <lewienato@gmail.com>
2022-12-18 12:11:28 +1100

    Inline static member.

--- src/WardenLua.h
+++ src/WardenLua.h
@@ -24,7 +24,7 @@ public:
     static bool HandleWLPayload(ChatHandler* handler, std::string payload);
     static bool HandleWLCPayload(ChatHandler* handler);
 private:
-    static uint32 payloadId = 44111;
+    inline static uint32 payloadId= 44111;
 };
 
 #endif //MODULE_WARDENLUA_H
